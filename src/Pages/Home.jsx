65637a38f04bd70c5449ecb4d31a7950d20066e8
DYuHLin <damianver2000@gmail.com>
2024-05-07 17:50:31 +0200

    Made several changes, such as hiding some information and changing the address.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,25 +2,42 @@ import { useContext, useEffect, useState } from 'react'
 import {BrowserRouter as Router, Route, Routes, Navigate, Link} from 'react-router-dom'
 import UserContext from '../UserContext';
 import axios from 'axios';
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function Home() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { user, setUser } = useContext(UserContext);
 
-  useEffect(() => {
-    axios({method:'GET', url:"/api"}, {headers: { "Content-Type": "application/json" }})
+  const getBlogs = () => {
+    setLoading(true);
+    try{
+      axios({method:'GET', url:"/api"}, {headers: { "Content-Type": "application/json" }})
         .then(res => {
           setPosts(res.data);
         })
         .catch(err => console.log(err));  
-    
-  }, [posts]);
+      setLoading(false);
+    } catch(err){
+      toast.error(err.message);
+      setLoading(false);
+    };
+  };
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
 
   return (
     
     <section>
       <h1 className='home-title'>Welcome to my blog site</h1>
-      {posts.length === 0 ? <p>There are no posts</p> :
+      {
+        loading && <p>Fetching blogs...</p>
+      }
+      
+      {!loading && posts.length === 0 ? <p>There are no posts</p> :
       posts.map((blog) => {
         return(
           <Link key={blog._id} to={`${blog._id}`} className="blog blog-title-home">
